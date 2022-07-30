3a3f5f5ed2da7e4f6fe2cec8c99834906eed7763
Chaemin-L <chaem12@kookmin.ac.kr>
2022-07-30 23:33:16 +0900

    1.17 useMemo study

--- begin-react/src/App_2.js
+++ begin-react/src/App_2.js
@@ -1,12 +1,19 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useMemo } from 'react';
 //import Counter from './Counter'
 //import InputSample from './InputSample'
 import UserList from './UserList'
 import CreateUser from './CreateUser'
 
-function App() {
+
+function countActiveUsers(user) {
+    console.log('활성 사용자 수를 세는 중...!');
+    return user.filter(user => user.active).length;
+};
+
+function App() {    
     const [inputs, setInputs] = useState({ username: '', email: '' });
     const { username, email } = inputs;
+    //const [count, setCount] = useState(0);
 
     const onChange = (e) => {
         const { name, value } = e.target;
@@ -37,7 +44,11 @@ function App() {
           active: false,
         }
     ]);
-    
+    /*
+    useEffect(() => {
+        setCount(countActiveUsers(users));
+    }, [users])
+    */
     const nextId = useRef(4);
     //const nextId = { current: 4 };
     const onCreate = () => {
@@ -64,10 +75,13 @@ function App() {
             )
         );
     }
+
+    const count = useMemo(() => countActiveUsers(users), [users]);
     return (
         <div>
             <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
             <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+            <div>활성 사용자 수: { count }</div>
         </div>
     );
 }
