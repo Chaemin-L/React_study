72a5f4850506fea5c1e1bf74c678542925d5cb20
Chaemin-L <chaem12@kookmin.ac.kr>
2022-08-08 01:03:56 +0900

    feat: Make text-length restrict

--- 01_bulls_and_cows/src/InputForm.js
+++ 01_bulls_and_cows/src/InputForm.js
@@ -4,7 +4,7 @@ import Button from './component/Button';
 export default function InputForm({onChange, onGuess}) {
     return (
         <div>
-            <input placeholder="서로 다른 세 개의 수 입력" onChange={onChange}  required/>
+            <input placeholder="서로 다른 세 개의 수 입력" maxLength={3} onChange={onChange}  required/>
             <Button onClick={onGuess}>💡</Button>
         </div>
     )
