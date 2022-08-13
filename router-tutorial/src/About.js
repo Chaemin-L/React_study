b5c46a9b7a49555675203f9063a27130724458f8
Chaemin-L <chaem12@kookmin.ac.kr>
2022-08-14 00:51:13 +0900

    5.2 Param and Query String study

--- router-tutorial/src/About.js
+++ router-tutorial/src/About.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 const About = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const detail = searchParams.get('detail') === 'true';
+
   return (
     <div>
       <h1>소개</h1>
-      <p>이 프로젝트는 리액트 라우터 기초를 실습해보는 예제 프로젝트랍니다.</p>
+          <p>이 프로젝트는 리액트 라우터 기초를 실습해보는 예제 프로젝트랍니다.</p>
+          {detail && <button onClick={()=>setSearchParams({detail:false})}>안보이게 하기</button>}
     </div>
   );
 };
--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,6 +1,7 @@
 import { Link, Routes, Route } from 'react-router-dom';
 import Home from "./Home";
 import About from './About';
+import Profile from './Profile';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/profiles/:username/:description" element={<Profile />} />
       </Routes>
     </div>
   );
